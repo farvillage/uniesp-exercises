bd92b29c33233e0f16984fdc23295227711abc03
Ygor Carvalho <ygorsoares2003@gmail.com>
2023-08-21 19:27:00 -0300

    te odeio intellij

---
+++ fernanda/E01.java
@@ -0,0 +1,11 @@
+public class E01 {
+    public static void e01(String[] args) {
+    double num1 = 40;
+    double num2 = 15.00;
+    double total;
+
+    total = num1 * num2;
+
+    System.out.println(total);
+    }
+}
