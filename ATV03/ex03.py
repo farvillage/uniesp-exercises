edc9fdaa357daa1a34e37f78e47c8660d1dfe281
Ygor Carvalho <ygorsoares2003@gmail.com>
2023-10-06 15:39:09 -0300

    Delete ATV03 directory

--- ATV03/ex03.py
+++
@@ -1,6 +0,0 @@
-frase = str.upper(input('Digite uma frase: ')).strip()
-fraseLimpa = str.replace(frase, ' ','')
-if fraseLimpa == fraseLimpa[::-1]:
-    print('é palíndromo')
-else:
-    print('não é palíndromo')
